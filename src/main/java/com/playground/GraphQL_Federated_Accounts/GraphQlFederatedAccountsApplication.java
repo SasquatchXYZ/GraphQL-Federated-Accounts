f8fd806b1b0eda2ef392c74634080a7fc2b75432
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-04 13:39:04 -0500

    Initial commit

---
+++ src/main/java/com/playground/GraphQL_Federated_Accounts/GraphQlFederatedAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.playground.GraphQL_Federated_Accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlFederatedAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlFederatedAccountsApplication.class, args);
+	}
+
+}
